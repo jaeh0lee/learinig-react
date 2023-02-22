55ddd31b807cd76fe67550363d685f14b87f795f
jaeh0lee <woghfghf@naver.com>
2023-02-22 21:02:32 +0900

    props 학습

--- my-app/src/components/Item.js
+++ my-app/src/components/Item.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './item.css'
 
-function Item() {
-  const number  = 231.12;
+function Item(props) {
+  
+  const month = props.date.toLocaleString('en-US', {month : 'long'})
+  const day = props.date.toLocaleString('en-US', {month : '2-digit'})
+  const year = props.date.getFullYear()
+
   return (
     <div className='expense-item'>
-     <div>Date</div>
+     <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+     </div>
      <div  className='expense-item__description' >
-      <h2>{number}</h2>
-      <div className='expense-item__price'>Amount</div>
+      <h2>{props.title}</h2>
+      <div className='expense-item__price'>{props.amount}</div>
      </div>
     </div>
   )
